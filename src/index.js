c4c931a6103b8b2b07753adc626ddad0a1a47a18
Inchara nk <incharank1@gmail.com>
2026-06-01 22:08:27 +0530

    learning datatypes

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+//# sourceMappingURL=index.js.map
