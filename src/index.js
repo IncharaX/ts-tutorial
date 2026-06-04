687deee5746e07f409952db7f721e80652f8b71a
Inchara nk <incharank1@gmail.com>
2026-06-04 22:48:16 +0530

    datatypes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 "use strict";
+/*. datatypes
+let sales=123_456_789;
+let course:string="TypeScript";
+let is_published:boolean=true;
+let level;*/
 Object.defineProperty(exports, "__esModule", { value: true });
-let sales = 123_456_789;
-let course = "TypeScript";
-let is_published = true;
-let level;
+/*tuples
+let user:[number,string]=[1,"Mosh"];
+*/
+const Small = 1;
+const Medium = 2;
+const Large = 3;
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 0] = "Small";
+    Size[Size["Medium"] = 1] = "Medium";
+    Size[Size["Large"] = 2] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Small;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
+/*. datatypes
 let sales=123_456_789;
 let course:string="TypeScript";
 let is_published:boolean=true;
-let level;
+let level;*/
+
+/*tuples
+let user:[number,string]=[1,"Mosh"];
+*/
+/*
+const Small=1;
+const Medium=2;
+const Large=3;
+
+enum Size{Small,Medium,Large};
+let mySize:Size=Size.Small;
+console.log(mySize);
+*/
+
+ const enum Size{Small=1,Medium,Large};
+let mySize:Size=Size.Small;
+console.log(mySize);
